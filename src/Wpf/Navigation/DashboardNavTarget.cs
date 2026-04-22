afa1fbe98b202e5012f40190cc4822830b0d2734
kwonikhyeon <kwen1028@naver.com>
2026-04-22 18:52:49 +0900

    feat(wpf): add DashboardNavTarget (Order=10)

---
+++ src/Wpf/Navigation/DashboardNavTarget.cs
@@ -0,0 +1,14 @@
+using CanMonitor.Wpf.Dashboard;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CanMonitor.Wpf.Navigation;
+
+public sealed class DashboardNavTarget : INavTarget
+{
+    public string Key => "Dashboard";
+    public string Title => "Dashboard";
+    public string IconGlyph => "BarChart4";
+    public int Order => 10;
+
+    public object CreateViewModel(IServiceProvider sp) => sp.GetRequiredService<DashboardViewModel>();
+}
---
+++ tests/Wpf.Tests/Navigation/DashboardNavTargetTests.cs
@@ -0,0 +1,33 @@
+using CanMonitor.Wpf.Dashboard;
+using CanMonitor.Wpf.Navigation;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace CanMonitor.Wpf.Tests.Navigation;
+
+public class DashboardNavTargetTests
+{
+    [Fact]
+    public void Properties_are_fixed()
+    {
+        var target = new DashboardNavTarget();
+        target.Key.Should().Be("Dashboard");
+        target.Title.Should().Be("Dashboard");
+        target.IconGlyph.Should().Be("BarChart4");
+        target.Order.Should().Be(10);
+    }
+
+    [Fact]
+    public void CreateViewModel_resolves_DashboardViewModel_from_sp()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton(new DashboardViewModel(Array.Empty<IDashboardWidget>()));
+        var sp = services.BuildServiceProvider();
+
+        var target = new DashboardNavTarget();
+        var vm = target.CreateViewModel(sp);
+
+        vm.Should().BeOfType<DashboardViewModel>();
+    }
+}
